ccadb905faf128442c38aebdc38d2fbe767092f8
Clairo Dorneles <clairo@Clairos-MacBook-Pro.local>
2021-02-12 08:57:39 -0300

    bug003: correct output file

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 ## azure resource group outputs
 
 output "rgName" {
-  value = azurerm.resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "location" {
-  value = azurerm.resource_group.rg.location
+  value = azurerm_resource_group.rg.location
 }
 
 output "tags" {
-  value = azurerm.resource_group.rg.tags
+  value = azurerm_resource_group.rg.tags
 }
 
